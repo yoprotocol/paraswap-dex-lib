6431e880496eec1d4da7ea5dfb23565100fb44c3
Szeth Vallano <0xSzeth@gmail.com>
2025-03-20 14:58:22 -0600

    chore: init bunni-v2

---
+++ src/dex/bunni-v2/config.ts
@@ -0,0 +1,9 @@
+import { DexParams } from './types';
+import { DexConfigMap } from '../../types';
+import { Network, SwapSide } from '../../constants';
+
+export const BunniV2Config: DexConfigMap<DexParams> = {
+  BunniV2: {
+    // TODO: complete me!
+  },
+};
