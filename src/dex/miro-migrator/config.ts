47b832d5c8b76729efd59e2cbf1c2155c25f10fd
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2025-09-09 18:19:07 +0300

    chore(config): update `MiroMigrator` contract addresses

--- src/dex/miro-migrator/config.ts
+++ src/dex/miro-migrator/config.ts
@@ -5,13 +5,13 @@ import { Network } from '../../constants';
 export const MiroMigratorConfig: DexConfigMap<DexParams> = {
   MiroMigrator: {
     [Network.MAINNET]: {
-      migratorAddress: '0x4aBD869acbF927048434d137A3331D006Db54416',
+      migratorAddress: '0xA10fA49197D1b9a48d7e06335D762733427e8fB9',
       vlrTokenAddress: '0x4e107a0000DB66f0E9Fd2039288Bf811dD1f9c74',
       pspTokenAddress: '0xcAfE001067cDEF266AfB7Eb5A286dCFD277f3dE5',
       sePsp1TokenAddress: '0x716fBC68E0c761684D9280484243FF094CC5FfAB',
     },
     [Network.OPTIMISM]: {
-      migratorAddress: '0xb6208dE484dFb4Fd54148b1B9a745E9dF29519Ee',
+      migratorAddress: '0xDE191e579bD718a81de99df45884440c3Df6b4f9',
       vlrTokenAddress: '0x4e107a0000DB66f0E9Fd2039288Bf811dD1f9c74',
       pspTokenAddress: '0xd3594E879B358F430E20F82bea61e83562d49D48',
       sePsp1TokenAddress: '0x8C934b7dBc782568d14ceaBbEAeDF37cB6348615',
