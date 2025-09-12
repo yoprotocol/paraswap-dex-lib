13989fa1ee57691c62f5b6a6592d69219020ad7f
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2025-09-12 13:46:36 +0300

    feat(config): add `MiroMigrator` `BSC` support

--- src/dex/miro-migrator/config.ts
+++ src/dex/miro-migrator/config.ts
@@ -16,5 +16,11 @@ export const MiroMigratorConfig: DexConfigMap<DexParams> = {
       pspTokenAddress: '0xd3594E879B358F430E20F82bea61e83562d49D48',
       sePsp1TokenAddress: '0x8C934b7dBc782568d14ceaBbEAeDF37cB6348615',
     },
+    [Network.BSC]: {
+      migratorAddress: '0xD6a861BD2F8eeD343b8CCDb977DE1d106c72F557',
+      vlrTokenAddress: '0x4e107a0000DB66f0E9Fd2039288Bf811dD1f9c74',
+      pspTokenAddress: '0xcAfE001067cDEF266AfB7Eb5A286dCFD277f3dE5',
+      sePsp1TokenAddress: '',
+    },
   },
 };
