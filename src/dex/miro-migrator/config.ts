46ac43602e8abe64a88b716c99e56f7b603757b5
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2025-09-17 10:21:27 +0300

    chore(style): lowercase `MiroMigrator` config addresses

--- src/dex/miro-migrator/config.ts
+++ src/dex/miro-migrator/config.ts
@@ -5,21 +5,21 @@ import { Network } from '../../constants';
 export const MiroMigratorConfig: DexConfigMap<DexParams> = {
   MiroMigrator: {
     [Network.MAINNET]: {
-      migratorAddress: '0x311BCA70b6cd9f320a68A63B2331d7a3Ca55BdE7',
-      vlrTokenAddress: '0x4e107a0000DB66f0E9Fd2039288Bf811dD1f9c74',
-      pspTokenAddress: '0xcAfE001067cDEF266AfB7Eb5A286dCFD277f3dE5',
-      sePsp1TokenAddress: '0x716fBC68E0c761684D9280484243FF094CC5FfAB',
+      migratorAddress: '0x311bca70b6cd9f320a68a63b2331d7a3ca55bde7',
+      vlrTokenAddress: '0x4e107a0000db66f0e9fd2039288bf811dd1f9c74',
+      pspTokenAddress: '0xcafe001067cdef266afb7eb5a286dcfd277f3de5',
+      sePsp1TokenAddress: '0x716fbc68e0c761684d9280484243ff094cc5ffab',
     },
     [Network.OPTIMISM]: {
-      migratorAddress: '0x3F06Aa8fFF196F9d5033553ca035aa09FAE6492c',
-      vlrTokenAddress: '0x4e107a0000DB66f0E9Fd2039288Bf811dD1f9c74',
-      pspTokenAddress: '0xd3594E879B358F430E20F82bea61e83562d49D48',
-      sePsp1TokenAddress: '0x8C934b7dBc782568d14ceaBbEAeDF37cB6348615',
+      migratorAddress: '0x3f06aa8fff196f9d5033553ca035aa09fae6492c',
+      vlrTokenAddress: '0x4e107a0000db66f0e9fd2039288bf811dd1f9c74',
+      pspTokenAddress: '0xd3594e879b358f430e20f82bea61e83562d49d48',
+      sePsp1TokenAddress: '0x8c934b7dbc782568d14ceabbeaedf37cb6348615',
     },
     [Network.BSC]: {
-      migratorAddress: '0xD6a861BD2F8eeD343b8CCDb977DE1d106c72F557',
-      vlrTokenAddress: '0x4e107a0000DB66f0E9Fd2039288Bf811dD1f9c74',
-      pspTokenAddress: '0xcAfE001067cDEF266AfB7Eb5A286dCFD277f3dE5',
+      migratorAddress: '0xd6a861bd2f8eed343b8ccdb977de1d106c72f557',
+      vlrTokenAddress: '0x4e107a0000db66f0e9fd2039288bf811dd1f9c74',
+      pspTokenAddress: '0xcafe001067cdef266afb7eb5a286dcfd277f3de5',
       sePsp1TokenAddress: '',
     },
   },
